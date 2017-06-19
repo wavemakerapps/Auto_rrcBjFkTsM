a46a37cd4c731859936a30723063d3f103ef865d
root <root@99aeb7ada0c8>
2017-06-19 09:21:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_rrcBjFkTsM",
+  "homePage" : "Main",
+  "name" : "Auto_rrcBjFkTsM",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
